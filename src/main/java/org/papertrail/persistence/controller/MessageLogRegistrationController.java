fdd228682a44de3086b23a55779c2eff7cd71b1d
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-08-13 21:31:21 +0530

    feat(api): add skeleton API controllers

---
+++ src/main/java/org/papertrail/persistence/controller/MessageLogRegistrationController.java
@@ -0,0 +1,11 @@
+package org.papertrail.persistence.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+@RequestMapping("/api/v1/log/message")
+public class MessageLogRegistrationController {
+}
