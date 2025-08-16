2596d0d19b5b58b011d2526767ebf8777ce629c0
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-08-16 15:46:48 +0530

    feat(api): configure specific cache expirations and null value handling
    This change introduces specific time-to-live (TTL) configurations for
    different cache regions, allowing for more granular control over data
    expiration. It also disables the caching of null values

--- src/main/java/org/papertrail/persistence/configuration/CacheConfig.java
+++ src/main/java/org/papertrail/persistence/configuration/CacheConfig.java
@@ -8,6 +8,10 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class CacheConfig {
 
@@ -18,10 +22,17 @@ public class CacheConfig {
                         RedisSerializationContext.SerializationPair.fromSerializer(
                                 new GenericJackson2JsonRedisSerializer()
                         )
-                );
+                ).disableCachingNullValues();
+
+        //per cache config
+        Map<String, RedisCacheConfiguration> perCacheConfig = new HashMap<>();
+        perCacheConfig.put("auditLog", config.entryTtl(Duration.ofDays(30)));
+        perCacheConfig.put("messageLog", config.entryTtl(Duration.ofDays(30)));
+        perCacheConfig.put("messageContent", config.entryTtl(Duration.ofMinutes(15)));
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
+                .withInitialCacheConfigurations(perCacheConfig)
                 .build();
     }
 }
